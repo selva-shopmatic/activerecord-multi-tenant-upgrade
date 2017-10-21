8ef8def9e80e0245c26dd2e6928d0219fdcb56cc
Liang-Chi Tseng <lctseng@cs.nctu.edu.tw>
2017-10-21 15:42:30 +0800

    do not use composite primary key

--- lib/activerecord-multi-tenant/migrations.rb
+++ lib/activerecord-multi-tenant/migrations.rb
@@ -34,18 +34,19 @@ if defined?(ActiveRecord::Migration)
   ActiveRecord::Migration.send(:include, MultiTenant::MigrationExtensions)
 end
 
-module ActiveRecord
-  module ConnectionAdapters # :nodoc:
-    module SchemaStatements
-      alias :orig_create_table :create_table
-      def create_table(table_name, options = {}, &block)
-        ret = orig_create_table(table_name, options.except(:partition_key), &block)
-        if options[:partition_key] && options[:partition_key].to_s != 'id'
-          execute "ALTER TABLE #{table_name} DROP CONSTRAINT #{table_name}_pkey"
-          execute "ALTER TABLE #{table_name} ADD PRIMARY KEY(id, \"#{options[:partition_key]}\")"
-        end
-        ret
-      end
-    end
-  end
-end
+# Shopmatic: We don't use composite primary key...
+# module ActiveRecord
+#   module ConnectionAdapters # :nodoc:
+#     module SchemaStatements
+#       alias :orig_create_table :create_table
+#       def create_table(table_name, options = {}, &block)
+#         ret = orig_create_table(table_name, options.except(:partition_key), &block)
+#         if options[:partition_key] && options[:partition_key].to_s != 'id'
+#           execute "ALTER TABLE #{table_name} DROP CONSTRAINT #{table_name}_pkey"
+#           execute "ALTER TABLE #{table_name} ADD PRIMARY KEY(id, \"#{options[:partition_key]}\")"
+#         end
+#         ret
+#       end
+#     end
+#   end
+# end
