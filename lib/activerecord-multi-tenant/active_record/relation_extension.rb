a10355bc539083090adccddf48e1eab9a05c6acb
svgiridharreddy <giridhar.reddy@goshopmatic.com>
2022-06-20 13:19:37 +0530

    updated multi_tenant_orig_initialize arguments according to rails 5

--- lib/activerecord-multi-tenant/active_record/relation_extension.rb
+++ lib/activerecord-multi-tenant/active_record/relation_extension.rb
@@ -8,7 +8,8 @@ module ActiveRecord
 
     alias :multi_tenant_orig_initialize :initialize
     def initialize(*args, &block)
-      multi_tenant_orig_initialize(*args, &block)
+      multi_tenant_orig_initialize(*args)
+      # multi_tenant_orig_initialize(*args, &block)
       @creating_tenant = MultiTenant.current_tenant_id
       @multi_tenant_disabled = MultiTenant.multi_tenant_disabled?
     end
